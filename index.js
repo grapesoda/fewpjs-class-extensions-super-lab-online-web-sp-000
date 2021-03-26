1993538f91b394a3d7f637863f3584968bc3ee1b
Tres Tarwater <trest03@gmail.com>
2021-03-26 20:08:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ class Tree {
     static  definition() {
       return "A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves."
     }
-  
   }
   
   class Deciduous extends Tree {
